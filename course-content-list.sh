0bead563f7e66c69f70cd43ab3f23815a3f9dcc7
yurifugi <yuri@yuri.eti.br>
2021-03-31 14:07:20 -0300

    added installer

--- course-content-list.sh
+++ course-content-list.sh
@@ -2,4 +2,4 @@
 
 FOLDER_NAME=$(pwd | awk -F "/" '{print $NF}')
 FILE_NAME=$FOLDER_NAME.ods
-find . | sort -V |egrep -v  "srt|vtt" | tee "$FILE_NAME"
+find . | sort -V |grep -E -v  "srt|vtt" | tee "$FILE_NAME"
