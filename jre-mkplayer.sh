a60c5da7f5135a242076230bfa02e1f3b9319969
yurifugi <yuri@yuri.eti.br>
2021-04-13 14:06:33 -0300

    jre-mkplayer now lists past episodes

--- jre-mkplayer.sh
+++ jre-mkplayer.sh
@@ -6,12 +6,21 @@
 
 # set -x
 
+SHOWURL="https://open.spotify.com/show/4rOoJ6Egrf8K2IrywzwOMk"
+TMPFILE=$(mktemp) || exit 1
+
 if [ "$1" = "" ]
 then
-    echo "Must provide episode id"
-    echo "Show: https://open.spotify.com/show/4rOoJ6Egrf8K2IrywzwOMk"
+    echo "Must provide episode id."
+    echo "Example: $ jre-mkplayer.sh 3Atye1uCqaW2Ver3d16wJO"
+    echo "Show: $SHOWURL"
     echo "Ep: https://open.spotify.com/episode/3Atye1uCqaW2Ver3d16wJO"
     echo "                                     ^--------------------^ < This is episode id"
+    echo 
+    echo "Getting past episodes..."
+    curl "$SHOWURL" --output "$TMPFILE" --silent
+    xmllint --nowarning --html "$TMPFILE" 2>/dev/null  |grep  -E 'track-name|tracklist-row' |head -20 | grep -oE 'href.*|\#.*' | cut -d\< -f1
+    rm -f "$TMPFILE"
     exit 1
 fi
 
