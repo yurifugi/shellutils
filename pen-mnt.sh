80c4b0076655cd70df65e10643d24cbc27e9dc30
yurifugi <yuri@yuri.eti.br>
2021-05-12 12:45:07 -0300

    pen-mount checks

--- pen-mnt.sh
+++ pen-mnt.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+
 penDev="/dev/sdb1"
 mountPoint="/mnt/pen"
 
-echo "sudo mount..."
+set -x
 sudo mount "$penDev" "$mountPoint"
+set +x
+
+if mountpoint -q "$mountPoint" 
+then
+   :
+else
+   echo "$mountPoint is not mounted. Aborting."
+   exit 1
+fi
+
 cd "$mountPoint" || return
 echo "Folder is" "$(pwd)"
 echo "Contents:"
-ls -lah
-df -kh |grep "$mountPoint" | awk '{printf("\n%s in %s capacity: %s. Free:  %s.\n", $1, $6, $2, $4) }'
+ls --indicator-style=slash --size --human-readable
+df -k --human-readable |grep "$mountPoint" | awk '{printf("\n%s in %s capacity: %s. Free:  %s.\n", $1, $6, $2, $4) }'
