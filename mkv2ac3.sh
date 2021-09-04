275a4c211376209d8785770e81d01caf70c4c40c
Yuri Fugi <yuri@yuri.eti.br>
2021-09-04 14:15:33 -0300

    new mkv2ac3

--- mkv2ac3.sh
+++ mkv2ac3.sh
@@ -1,64 +1,114 @@
 #!/usr/bin/env bash
 
-MEDIAMNT=$(lsblk -f -o NAME,MOUNTPOINT -l |grep media)
+if [ ! $# -ge 2 ]
+then
+    echo "Usage: $0 \"filename.[avi,mkv]\" \"/destination/folder/\""
+    exit 1
+fi
 
-echo "Device    MountPoint"
-echo "$MEDIAMNT"
+CONTINUE="n"
+while [ "$CONTINUE" != "y" ] 
+do
+    clear
+    echo "######## $0"
+    echo
+    echo "Destination will be: '$2$1'."
+    echo "Origin is '$(pwd)/$1'."
+    echo 
+    echo "Listing Video Streams from '$1'"
+    echo
+    ffprobe -i "$1" -hide_banner 2>&1 | grep -i "video" | nl -v0
+    echo
+    echo "Video Track is? (zero based)> "
+    read -r videoTrack
+    echo
+    echo "Video Track will be:"
+    ffprobe \
+        -hide_banner \
+        -v error \
+        -select_streams v:"$videoTrack" \
+        -show_entries stream \
+        "$1" 2>&1 | grep -i -e language -e codec_name -e codec_type -e duration
+    echo
+    echo "Ok? <y>"
+    read -n1 CONTINUE        
+done
 
-ls -la $(echo "$MEDIAMNT" | awk '{ print $2 }')
-
-printf "\nDestination: (/some/folder/)> "
-read -r DEST
 
 
-# Listing streams
-ffmpeg -i "$1" -f null 2>&1 | grep -iE "stream||title"
+CONTINUE="n"
+while [ "$CONTINUE" != "y" ] 
+do
+    clear
+    echo "######## $0"
+    echo
+    echo "Destination will be: '$2$1'."
+    echo "Origin is '$(pwd)/$1'."
+    echo 
+    echo "Listing Audio Streams from '$1'"
+    echo
+    ffprobe -i "$1" -hide_banner 2>&1 | grep -i "audio" | nl -v0
+    echo
+    echo "Audio Track is? (zero based)> "
+    read -r audioTrack
+    echo
+    echo "Audio Track will be:"
+    ffprobe \
+        -hide_banner \
+        -v error \
+        -select_streams a:"$audioTrack" \
+        -show_entries stream \
+        "$1" | grep -i -e language -e codec_name -e codec_type -e duration
+    echo
+    echo "Ok? <y>"
+    read -n1 CONTINUE        
+done
 
 
-# Converts audio track to ac3
-# Copy only the specified subtitle and video tracks
+CONTINUE="n"
+while [ "$CONTINUE" != "y" ] 
+do
+    clear
+    echo "######## $0"
+    echo
+    echo "Destination will be: '$2$1'."
+    echo "Origin is '$(pwd)/$1'."
+    echo 
+    echo "Listing Subtitle Streams (and its titles) from '$1'"
+    echo
+    ffprobe -i "$1" -hide_banner 2>&1 | grep -iE "subtitle|title" | nl -v0 -bpStream
+    echo
+    echo "Subtitle Track is? (zero based)> "
+    read -r subTrack
+    echo
+    echo "Subtitle Track will be:"
+    ffprobe \
+        -hide_banner \
+        -v error \
+        -select_streams s:"$subTrack" \
+        -show_entries stream \
+        "$1" | grep -i -e language -e title
+    echo
+    echo "Ok? <y>"
+    read -n1 CONTINUE        
+done
 
-printf "\nVideo Track is? (zero based)> "
-read -r videoTrack
-printf "\nAudio Track is? (zero based)> "
-read -r audioTrack
-printf "\nSubtitle Track is? (zero based)> "
-read -r subTrack
-
-printf "\nVideo:\n"
-ffprobe \
-    -hide_banner \
-    -v panic \
-    -select_streams v:"$videoTrack" \
-    -show_entries stream \
-    "$1" | grep lang
-
-printf "\nAudio:\n"
-ffprobe \
-    -hide_banner \
-    -v panic \
-    -select_streams a:"$audioTrack" \
-    -show_entries stream \
-    "$1" | grep lang
-
-printf "\nSubtitle:\n"
-ffprobe \
-    -loglevel error \
-    -select_streams s:24 \
-    -show_entries stream=index:stream_tags=language,title \
-    -of csv=p=0 \
-    "$1"
-
-printf "n\All's good? <y/n>"
-read CONTINUE
-
-if [[ $CONTINUE == "y" ]]
-then 
-    ffmpeg -i "$1" \
+if [[ ${#videoTrack} -gt 0 ]] && [[ ${#audioTrack} -gt 0 ]] && [[ ${#subTrack} -gt 0 ]]
+then
+        ffmpeg -i "$1" \
+        -hide_banner \
         -map 0:a:"$audioTrack" \
         -map 0:s:"$subTrack" \
         -map 0:v:"$videoTrack" \
         -c:v copy \
         -c:a ac3 \
-        -c:s copy "$DEST/$1"
+        -c:s copy "$2/$1"
+else
+    echo
+    echo "Something went wrong." 
+    echo "videoTrack: '$videoTrack'."
+    echo "audioTrack: '$audioTrack'."
+    echo "subTrack: '$subTrack'."
+
 fi
+
