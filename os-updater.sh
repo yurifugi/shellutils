274fe6934bd40c0308a6b9e0084b860fa178f063
Yuri Fugi <yuri@yuri.eti.br>
2023-10-23 22:19:17 -0300

    fix: removed "----"

--- os-updater.sh
+++ os-updater.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/bin/logger -p local0.notice -t os-updater -------- Update Started -------- 
+/usr/bin/logger -p local0.notice -t os-updater Update Started 
 
 /usr/bin/logger -p local0.notice -t os-updater Started apt-get update
 /usr/bin/apt-get update | /usr/bin/logger -p local0.notice -t os-updater 
@@ -14,4 +14,4 @@
 /usr/bin/logger -p local0.notice -t os-updater Started apt-get clean
 /usr/bin/apt-get clean -y 2>&1  | /usr/bin/logger -p local0.notice -t os-updater
 
-/usr/bin/logger -p local0.notice -t os-updater -------- Update Finished ------- 
+/usr/bin/logger -p local0.notice -t os-updater Update Finished 
