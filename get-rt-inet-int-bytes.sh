fa60809676498dcbf14b0a41e6606c1598f3cebb
Yuri Fugi <yuri@yuri.eti.br>
2023-11-02 15:11:10 -0300

    feat: added get-rt-inet-bytes

---
+++ get-rt-inet-int-bytes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo $(date --iso-8601=seconds;\
+    sshpass \
+        -p "$1" ssh \
+        -o StrictHostKeyChecking=no \
+        admin@10.1.1.1 \
+        ip -s link show vlan1  | \
+        grep -v bytes | \
+        sed -n 3,4p | \
+        sed 's/^[ \t]*//' | \
+        cut -d" " -f1 | \
+        xargs) > /mnt/sda1/stats/router-internet.csv
