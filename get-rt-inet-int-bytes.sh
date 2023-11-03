2b47960df9eeb1939f33629bba0387a56f373cc0
Yuri Fugi <yuri@yuri.eti.br>
2023-11-03 15:06:47 -0300

    feat: added get-rt-inet-bytes

--- get-rt-inet-int-bytes.sh
+++ get-rt-inet-int-bytes.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
-echo -e "$(sshpass \
-        -p "$1" ssh \
+ROUTER_RETURN=$(sshpass \
+        -p "$PASSWORD" ssh \
         -o StrictHostKeyChecking=no \
-        admin@10.1.1.1 \
-        ip -s link show vlan1  | \
-        grep -v bytes | \
-        sed -n 3,4p | \
-        sed 's/^[ \t]*//' | \
-        cut -d" " -f1);\
-        \n 1 \
-        \n router_10_1_1_1" >> /mnt/sda1/stats/router-internet.csv
+        "admin@10.1.1.1" \
+        'echo -n $(ip -o -s link show vlan1); \
+        echo -n "\\ $(cat /proc/uptime)"; \
+        echo -n "\\ RT-AC59U"' \
+        | tr -s ' ')
+        #  | \
+        # xargs)"
+echo $ROUTER_RETURN
+
+LINE1=$(echo "$ROUTER_RETURN" | cut -d"\\" -f4)
+LINE2=$(echo "$ROUTER_RETURN" | cut -d"\\" -f6)
+LINE3=$(echo "$ROUTER_RETURN" | cut -d"\\" -f7)
+LINE4=$(echo "$ROUTER_RETURN" | cut -d"\\" -f8)
+
+
+VAL_INBOUND="$(echo $LINE1 | cut -d' ' -f1)"
+VAL_OUTBOUND="$(echo $LINE2 | cut -d' ' -f1)"
+VAL_UPTIME="$(echo $LINE3 | cut -d' ' -f1)"
+VAL_HOSTNAME="$(echo $LINE4)"
+
+echo "\"$VAL_INBOUND\""
+echo "\"$VAL_OUTBOUND\""
+echo "\"$VAL_UPTIME\""
+echo ""\$VAL_HOSTNAME\""
